8de822047f2ec869bef296b714fca9d148d17c4a
David lai <davidviky303132@gmail.com>
2024-02-22 11:00:21 +0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,22 @@
 <script>
-  import App from '../components/App.svelte';
+    import App from '../components/App.svelte';
 </script>
 
+<h1 class="page-title">2022 - 2023 NBA Player Stats</h1>
+
 <App />
+
+<style>
+  .page-title {
+  display: block;
+  width: 100%;
+  text-align: center; 
+  margin-top: 0; 
+  color: white; 
+  font-family: 'Open Sans', sans-serif; 
+  background-color: #04549c;
+  margin-bottom: -5px;
+  padding: 20px 0;
+}
+
+</style>
